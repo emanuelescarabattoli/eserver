85121a93e2ee85636f767aaca691ba9abcf97113
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-07-03 10:40:25 +0200

    Changed STATE command

--- server.py
+++ server.py
@@ -1,30 +1,29 @@
 import socketserver
 
 
-status = "LIGHTGROUP1#OFF;LIGHTGROUP2#OFF;LIGHTGROUP3#OFF;GATE#CLOSED;SENSOR1#24.000;SENSOR2#30.000"
+state = "LIGHTGROUP1#OFF;LIGHTGROUP2#OFF;LIGHTGROUP3#OFF;GATE#CLOSED;SENSOR1#24.000;SENSOR2#30.000"
 
 class Handler(socketserver.BaseRequestHandler):
 
-
     def handle(self):
-        global status
+        global state
         self.data = self.request.recv(1024).strip().decode("utf-8")
-        if self.data == "STATUS":
-            self.request.sendall(bytes(status, "utf-8"))
+        if self.data == "STATE":
+            self.request.sendall(bytes(state, "utf-8"))
         else:
             received = self.data.split("#")
             received_device = received[0]
-            received_status = received[1]
+            received_state = received[1]
             new_stauts = ""
-            devices_statuses = status.split(";")
-            for device_status in devices_statuses:
-                device_data = device_status.split("#")
+            devices_statees = state.split(";")
+            for device_state in devices_statees:
+                device_data = device_state.split("#")
                 if device_data[0] == received_device:
-                    new_stauts += received_device + "#" + received_status + ";"
+                    new_stauts += received_device + "#" + received_state + ";"
                 else:
                     new_stauts += device_data[0] + "#" + device_data[1] + ";"
             new_stauts = new_stauts[:-1]
-            status = new_stauts
+            state = new_stauts
             self.request.sendall(bytes("DONE", "utf-8"))
 
 if __name__ == "__main__":
